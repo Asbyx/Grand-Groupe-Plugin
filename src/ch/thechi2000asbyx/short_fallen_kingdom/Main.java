3b885567b0d45f34d46fb68257688dfc7aa98570
Eugène <elrbergeron@gmail.com>
2021-05-24 11:22:29 +0200

    iniit fallen kingdom

---
+++ src/ch/thechi2000asbyx/short_fallen_kingdom/Main.java
@@ -0,0 +1,13 @@
+package ch.thechi2000asbyx.short_fallen_kingdom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
