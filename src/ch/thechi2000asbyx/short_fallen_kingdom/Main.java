a60a5fe53fd9a6c8df62f1539017983e9247c952
Eugène <elrbergeron@gmail.com>
2021-05-25 12:07:37 +0200

    Main broadcast Update

--- src/ch/thechi2000asbyx/short_fallen_kingdom/Main.java
+++ src/ch/thechi2000asbyx/short_fallen_kingdom/Main.java
@@ -1,25 +1,27 @@
 package ch.thechi2000asbyx.short_fallen_kingdom;
 
-import ch.thechi2000asbyx.common.Coordinates;
 import ch.thechi2000asbyx.short_fallen_kingdom.Teams.*;
 import org.bukkit.*;
 import org.bukkit.command.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public class Main extends JavaPlugin
 {
 	public static JavaPlugin PLUGIN;
-	
+	public static BukkitScheduler SCHEDULER;
+	private int eventsId;
+
 	@Override
 	public void onEnable()
 	{
 		PLUGIN = this;
-		
+		SCHEDULER = getServer().getScheduler();
+		eventsId = -1;
+
 		Bukkit.getPluginManager().registerEvents(new FlagEvents(), this);
 		Bukkit.getPluginManager().registerEvents(new BuildEvents(), this);
 		getCommand("fkteam").setExecutor(new FKTeamCommands());
-		
-		Coordinates c = Coordinates.fromString("[1, 2, 3]");
 	}
 	
 	@Override
@@ -35,12 +37,16 @@ public class Main extends JavaPlugin
 			case "startGame":
 				try
 				{
-					if (args.length != 5) throw new NumberFormatException();
+					if (args.length != 6 && args.length != 1) throw new NumberFormatException();
 					for (String s : args)
 					{
-						Integer.parseInt(s);
+						if(!s.equals("default")) Integer.parseInt(s);
 					}
-					getServer().getPluginManager().registerEvents(new EventsManager(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]), Integer.parseInt(args[4])), this);
+
+					EventsManager events;
+					if(args[0].equals("default")) events = new EventsManager();
+					else events = new EventsManager(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
+					eventsId = SCHEDULER.runTaskTimer(PLUGIN, () -> events.update(getServer().getWorld("world").getGameTime()), 0, 1).getTaskId();
 				}
 				catch (NumberFormatException e)
 				{
@@ -49,19 +55,29 @@ public class Main extends JavaPlugin
 							"<minimum number of seconds between each random chest>\n" +
 							"<maximum number of seconds between each random chest>\n" +
 							"<number of nights between each blood night> \n" +
-							"<radius of random events, center is the server spawn location> (must include both bases, or stop just in front of them)");
+							"<radius of random events, center is the server spawn location> (must include both bases, or stop just in front of them)\n" +
+							"<day where PvP is allowed>" + ChatColor.WHITE);
 				}
 				break;
 			
 			case "stopGame":
-				//fixme
+				if(eventsId == -1) {
+					sender.sendMessage(ChatColor.RED + "The game hasn't start yet !");
+					break;
+				}
+				broadcast("The game has been stopped !");
+				SCHEDULER.cancelTask(eventsId);
 				break;
 			
 			case "middleChest":
-				EventsManager.initMiddleChest();
+				EventsManager.initMiddleChest(getServer().getWorld("world"));
 				break;
 		}
 		
 		return true;
+	}
+
+	public static void broadcast(String arg) {
+		getPlugin(Main.class).getServer().broadcastMessage(ChatColor.BLUE + "[Server] " + arg + ChatColor.WHITE);
 	}
 }
