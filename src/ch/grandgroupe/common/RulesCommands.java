5aa797c839953eec0d9c7401a706a565b827e640
Ludovic Mermod <ludovic.mermod@epfl.ch>
2021-06-20 09:06:16 +0200

    Improve JavaDoc

--- src/ch/grandgroupe/common/RulesCommands.java
+++ src/ch/grandgroupe/common/RulesCommands.java
@@ -12,20 +12,20 @@ import java.util.*;
  */
 public final class RulesCommands implements CommandExecutor
 {
-	private final Map<String, AbstractListener> listeners = new TreeMap<>();
+	private final Map<String, AbstractListener> features = new TreeMap<>();
 	
 	/**
 	 * Default constructor: initiate all listeners, disabled by default
 	 */
 	public RulesCommands() {
-		listeners.put("tntBow", new TntBow());
-		listeners.put("nudeBow", new NudeBow());
-		listeners.put("harvester", new Harvester());
-		listeners.put("hook", new Hook());
-		listeners.put("compassTargeting", new CompassTargeting());
-		listeners.put("tomb", new Tomb());
+		features.put("tntBow", new TntBow());
+		features.put("nudeBow", new NudeBow());
+		features.put("harvester", new Harvester());
+		features.put("hook", new Hook());
+		features.put("compassTargeting", new CompassTargeting());
+		features.put("tomb", new Tomb());
 		
-		listeners.values().forEach(abstractListener -> {
+		features.values().forEach(abstractListener -> {
 			abstractListener.disable();
 			Bukkit.getPluginManager().registerEvents(abstractListener, Main.PLUGIN);
 		});
@@ -33,77 +33,80 @@ public final class RulesCommands implements CommandExecutor
 	
 	@SuppressWarnings("SpellCheckingInspection")
 	@Override
-	public boolean onCommand(CommandSender sender, @Nonnull Command command,@Nonnull String label, String[] arg) {
+	public boolean onCommand(CommandSender sender, @Nonnull Command command, @Nonnull String label, String[] arg) {
 		if (sender.isOp()) {
-			switch (arg[0].toLowerCase()) {
-				case "tntbow":
-					if (arg[1].equalsIgnoreCase("true") || arg[1].equalsIgnoreCase("false")) {
-						enabler(Boolean.parseBoolean(arg[1]), "tntBow", "Tnt Bow");
-					}
-					else {
-						try {
-							((TntBow) listeners.get("tntBow")).setPower(Float.parseFloat(arg[1]));
-							Main.broadcast("Power of Tnt bow set to: " + ChatColor.GOLD + Float.parseFloat(arg[1]));
+			if (arg.length == 2) {
+				switch (arg[0].toLowerCase()) {
+					case "tntbow":
+						if (arg[1].equalsIgnoreCase("true") || arg[1].equalsIgnoreCase("false")) {
+							enabler(Boolean.parseBoolean(arg[1]), "tntBow", "Tnt Bow");
 						}
-						catch (NumberFormatException e) {
-							sender.sendMessage(ChatColor.RED + "Invalid arguments: boolean (enabling) or float value (set the power)");
+						else {
+							try {
+								((TntBow) features.get("tntBow")).setPower(Float.parseFloat(arg[1]));
+								Main.broadcast("Power of Tnt bow set to: " + ChatColor.GOLD + Float.parseFloat(arg[1]));
+							}
+							catch (NumberFormatException e) {
+								sender.sendMessage(ChatColor.RED + "Invalid arguments: boolean (enabling) or float value (set the power)");
+							}
 						}
-					}
-					break;
-				
-				case "nudebow":
-					enabler(Boolean.parseBoolean(arg[1]), "nudeBow", "Nude Bow");
-					break;
-				
-				case "harvester":
-					if (arg[1].equalsIgnoreCase("true") || arg[1].equalsIgnoreCase("false")) {
-						enabler(Boolean.parseBoolean(arg[1]), "harvester", "Harvester");
-					}
-					else {
-						try {
-							((Harvester) listeners.get("harvester")).setRadius(Integer.parseInt(arg[1]));
-							Main.broadcast("Radius of action of the harvester set to: " + ChatColor.GOLD + Integer.parseInt(arg[1]));
+						break;
+					
+					case "nudebow":
+						enabler(Boolean.parseBoolean(arg[1]), "nudeBow", "Nude Bow");
+						break;
+					
+					case "harvester":
+						if (arg[1].equalsIgnoreCase("true") || arg[1].equalsIgnoreCase("false")) {
+							enabler(Boolean.parseBoolean(arg[1]), "harvester", "Harvester");
 						}
-						catch (NumberFormatException e) {
-							sender.sendMessage(ChatColor.RED + "Invalid arguments: boolean (enabling) or float value (set the radius)");
+						else {
+							try {
+								((Harvester) features.get("harvester")).setRadius(Integer.parseInt(arg[1]));
+								Main.broadcast("Radius of action of the harvester set to: " + ChatColor.GOLD + Integer.parseInt(arg[1]));
+							}
+							catch (NumberFormatException e) {
+								sender.sendMessage(ChatColor.RED + "Invalid arguments: boolean (enabling) or float value (set the radius)");
+							}
 						}
-					}
-					break;
-				
-				case "hook":
-					enabler(Boolean.parseBoolean(arg[1]), "hook", "Hook");
-					break;
-				
-				case "compasstargeting":
-					enabler(Boolean.parseBoolean(arg[1]), "compassTargeting", "Compass Targeting");
-					break;
-				
-				case "tomb":
-					enabler(Boolean.parseBoolean(arg[1]), "tomb", "Tomb");
-					break;
+						break;
+					
+					case "hook":
+						enabler(Boolean.parseBoolean(arg[1]), "hook", "Hook");
+						break;
+					
+					case "compasstargeting":
+						enabler(Boolean.parseBoolean(arg[1]), "compassTargeting", "Compass Targeting");
+						break;
+					
+					case "tomb":
+						enabler(Boolean.parseBoolean(arg[1]), "tomb", "Tomb");
+						break;
+				}
 			}
+			else
+				sender.sendMessage(ChatColor.RED + "Invalid command parameters");
 		}
+		else
+			sender.sendMessage(ChatColor.RED + "You are not allowed to use this commmand");
 		return true;
 	}
 	
-	//method to be called for basic enable/disable event: new value = enable value | index = index in the list | name = name of the Listener for the players
+	/**
+	 * Enable/Disable a feature (it has to be registered in the features map
+	 *
+	 * @param newValue the new state of the feature (true => enable and false => disable)
+	 * @param key      the key of the feature in the map
+	 * @param name     the name of the feature (to be printed in the chat)
+	 */
 	private void enabler(boolean newValue, String key, String name) {
 		if (newValue) {
-			listeners.get(key).enable();
-			enable(name);
+			features.get(key).enable();
+			Main.broadcast(name + ChatColor.GREEN + " enabled");
 		}
 		else {
-			listeners.get(key).disable();
-			disable(name);
+			features.get(key).disable();
+			Main.broadcast(name + ChatColor.RED + " disabled");
 		}
-	}
-	
-	//do not use
-	private void disable(String str) {
-		Main.broadcast(str + ChatColor.RED + " disabled");
-	}
-	
-	private void enable(String str) {
-		Main.broadcast(str + ChatColor.GREEN + " enabled");
 	}
 }
